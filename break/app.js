e529b852290e784e1fdb9f7d0c65666658a3ba5d
Moath <m.ahmad@sit-mena.com>
2022-02-24 16:15:00 +0200

    editing the code and adding function to it

--- break/app.js
+++ break/app.js
@@ -4,6 +4,7 @@ var products = [
     { name: 'tablet 1', price: 900 },
     { name: 'laptop', price: 1200 },
     { name: 'tablet 2', price: 900 },
+    { name: 'headset', price: 300 },
 ];
 // for (let i = 0; i < products.length; i++) {
 //     if (products[i].price == 900) {
@@ -11,8 +12,35 @@ var products = [
 //         break;
 //     }
 // }
-for (var i = 0; i < products.length; i++) {
-    if (products[i].price == 900) {
-        console.log("We find this item (".concat(products[i].name, ") which is suit your selected price (").concat(products[i].price, ")"));
-    }
+// for (let i = 0; i < products.length; i++) {
+//     if (products[i].price == 900) {
+//         console.log(`We find this item (${products[i].name}) which is suit your selected price (${products[i].price})`)
+//     }
+// }
+var discound = 0;
+var product = products[3];
+switch (product.name) {
+    case 'phone':
+        discound = 5;
+        showDiscount(product.name, discound);
+        break;
+    case 'tablet 1':
+        discound = 10;
+        showDiscount(product.name, discound);
+        break;
+    case 'laptop':
+        discound = 15;
+        showDiscount(product.name, discound);
+        break;
+    case 'tablet 2':
+        discound = 20;
+        showDiscount(product.name, discound);
+        break;
+    default:
+        // throw Error(`The product doesn't have a discount`);
+        console.log("".concat(product.name, " doesn't have a discount"));
+}
+;
+function showDiscount(name, discound) {
+    console.log("".concat(name, " has discount ").concat(discound));
 }
--- break/app.ts
+++ break/app.ts
@@ -5,6 +5,7 @@ let products = [
     { name: 'tablet 1', price: 900 },
     { name: 'laptop', price: 1200 },
     { name: 'tablet 2', price: 900 },
+    { name: 'headset', price: 300 },
 ];
 
 // for (let i = 0; i < products.length; i++) {
@@ -14,8 +15,37 @@ let products = [
 //     }
 // }
 
-for (let i = 0; i < products.length; i++) {
-    if (products[i].price == 900) {
-        console.log(`We find this item (${products[i].name}) which is suit your selected price (${products[i].price})`)
-    }
+// for (let i = 0; i < products.length; i++) {
+//     if (products[i].price == 900) {
+//         console.log(`We find this item (${products[i].name}) which is suit your selected price (${products[i].price})`)
+//     }
+// }
+
+let discound = 0;
+let product = products[3];
+
+switch (product.name) {
+    case 'phone':
+        discound = 5;
+        showDiscount(product.name, discound);
+        break
+    case 'tablet 1':
+        discound = 10;
+        showDiscount(product.name, discound);
+        break;
+    case 'laptop':
+        discound = 15;
+        showDiscount(product.name, discound);
+        break;
+    case 'tablet 2':
+        discound = 20;
+        showDiscount(product.name, discound);
+        break;
+    default:
+        // throw Error(`The product doesn't have a discount`);
+        console.log(`${product.name} doesn't have a discount`);
+};
+
+function showDiscount(name, discound) {
+    console.log(`${name} has discount ${discound}`);
 }
