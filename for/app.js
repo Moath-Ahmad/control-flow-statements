296a49b7afcdedc13504394448e07ecad5dd7862
Moath <m.ahmad@sit-mena.com>
2022-02-24 14:18:05 +0200

    Adding for

---
+++ for/app.js
@@ -0,0 +1,26 @@
+// for loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// let i = 0;
+// for (; i < 10; i++) {
+//     console.log(i)
+// }
+// for (let i = 0; ; i++) {
+//     console.log(i);
+//     if (i > 8) break;
+// }
+for (var i = 0;; i++) {
+    if (i < 10) {
+        console.log(i);
+    }
+    else {
+        break;
+    }
+}
+// let i = 0;
+// for (; ;) {
+//     console.log(i);
+//     i++;
+//     if (i > 9) break;
+// }
