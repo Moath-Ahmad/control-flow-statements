9204353e6f84994d56429992afbb4b127804e139
Moath <m.ahmad@sit-mena.com>
2022-02-24 15:16:03 +0200

    Adding do-while example

---
+++ do-while/app.ts
@@ -0,0 +1,9 @@
+
+// in do-while the statment between the curly braces will start to do code inside it before checking the condition at least one time.
+
+let i = 10;
+
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
